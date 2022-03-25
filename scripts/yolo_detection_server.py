ec0c84a991e9bf43492b5408a50b88494b4bf298
Artur Istvan Karoly <artur.karoly@irob.uni-obuda.hu>
2022-03-25 19:33:26 +0100

    Add documentation

--- scripts/yolo_detection_server.py
+++ scripts/yolo_detection_server.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python
 
+## @package yolo_bounding_box_detection
+# The ROS node for detecting bounding boxes with the YOLO model
+#
+# Defines a ROS action server for detecting bounding boxes with the YOLO model. The yolov5 repository from https://github.com/ultralytics/yolov5 has to be pulled next to this package
+
 import rospy
 import actionlib
 import rospkg
@@ -13,8 +18,20 @@ import sys
 import torch
 
 
+## BoundingBoxDetectionServer class
+#
+# Defines a ROS action server for predicting bounding boxes with the YOLO model
 class BoundingBoxDetectionServer:
+  '''
+    Class for YOLO predictions
+  '''
+
+  ## Constructor of BoundingBoxDetectionServer class
+  # @param model_name string type name of the file containing the trained model's weights (pt file in the 'models' directory of this repository)
   def __init__(self, model_name="best"):
+
+    ## @var rospack
+    #  rospkg.RosPack() object for getting ROS package paths
     self.rospack = rospkg.RosPack()
     rospackage_root = self.rospack.get_path("yolo_bounding_box_detection")
 
@@ -24,22 +41,52 @@ class BoundingBoxDetectionServer:
     from utils.augmentations import letterbox
     from utils.general import non_max_suppression, scale_coords, xyxy2xywh
 
+    ## @var letterbox
+    #  letterbox function from yolov5/utils/augmentations
     self.letterbox = letterbox
+
+    ## @var non_max_suppression
+    #  non_max_suppression function from yolov5/utils/general
     self.non_max_suppression = non_max_suppression
+
+    ## @var scale_coords
+    #  scale_coords function from yolov5/utils/general
     self.scale_coords = scale_coords
+
+    ## @var xyxy2xywh
+    #  xyxy2xywh function from yolov5/utils/general
     self.xyxy2xywh = xyxy2xywh
 
+    ## @var device
+    #  Hardware acceleration (CPU)
     self.device = select_device('')
+
     weights_file_path = os.path.join(rospackage_root, "models", model_name + ".pt")
+
+    ## @var model
+    #  the YOLO model
     self.model = DetectMultiBackend(weights_file_path, device=self.device, dnn=False)
     rospy.loginfo("YOLO model loaded: " + model_name + ".pt")
 
+    ## @var bridge
+    #  CvBridge() object for conversions between numpy arrays and ROS Image message types
     self.bridge = CvBridge()
+
+    ## @var server
+    #  The ROS action server for the BoundingBoxDetectionAction action. The name of the action is "bounding_box_detection"
     self.server = actionlib.SimpleActionServer('bounding_box_detection', BoundingBoxDetectionAction, self.execute, False)
     self.server.start()
 
+
+  ## BoundingBoxDetectionAction callback
+  # This function gets called whenever the ROS action server receives a goal from a client
+  # @param goal bark_msgs/BoundingBoxDetectionGoal type action goal, it contains an image for bounding box detection (see action definition for further details)
   def execute(self, goal):
-    # Do lots of awesome groundbreaking robot stuff here
+    '''
+    BoundingBoxDetectionAction callback
+
+    goal: bark_msgs/BoundingBoxDetectionGoal, action goal, it contains an image for bounding box detection (see action definition for further details)
+    '''
     img = self.bridge.imgmsg_to_cv2(goal.image, desired_encoding='bgr8')
     # Get greater dimension of image
     img_larger_shape = img.shape[1] if img.shape[1] >= img.shape[0] else img.shape[0]
